5bb007100d391795c27dfc6f4f358a392faf12f0
Uday. V Krishna <vamshi8724@.gmail.com>
2022-10-26 15:47:34 +0530

    swagger bean config

--- src/main/java/vm/money/track/TrackApplication.java
+++ src/main/java/vm/money/track/TrackApplication.java
@@ -10,6 +10,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -21,10 +25,16 @@ public class TrackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TrackApplication.class, args);
 	}
-//	@GetMapping("/")
+	@GetMapping("/")
 	public String hello(){
 		return "hello vm";
 	}
+	@Bean
+	public Docket productApi() {
+	    return new Docket(DocumentationType.SWAGGER_2).select()
+	            .apis(RequestHandlerSelectors.basePackage("vm.money.track"))
+	            .build();
+   }
 
 //    @Bean
 //    DataSource getDataSource() {
